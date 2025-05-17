1dfc632fe9573691908849d17c44b5428dfc5ccb
Stevanus Cahya Anggara <stevanuschyaanggara@gmail.com>
2025-05-17 11:54:58 +0700

    zzz

--- app/Http/Controllers/QrCodeController.php
+++ app/Http/Controllers/QrCodeController.php
@@ -8,7 +8,7 @@ use App\Models\QrCode as QrcodeModel;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use App\Models\Anggota;
 
-class QrCodeController extends Controller
+class QrcodeController extends Controller
 {
     public function welcome()
     {
